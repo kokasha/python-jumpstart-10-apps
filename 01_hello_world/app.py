ba81ba244c5a52d8975804cda948ed5ed28e2389
Karim Okasha <k.okasha@gmail.com>
2018-10-09 06:20:00 +1100

    Adding First App

---
+++ 01_hello_world/app.py
@@ -0,0 +1,7 @@
+print('------------------------------')
+print('         Hello World')
+print('------------------------------')
+print()
+user_input = input('What is your name?')
+greeting = 'How are you '
+print(greeting + user_input)
