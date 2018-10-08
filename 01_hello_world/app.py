8a37f201849a1474af713529d07d8c39f75f7d57
Karim Okasha <k.okasha@gmail.com>
2018-10-09 06:26:17 +1100

    Modified the hello world

--- 01_hello_world/app.py
+++ 01_hello_world/app.py
@@ -1,7 +1,7 @@
-print('------------------------------')
+print('-' * 30)
 print('         Hello World')
-print('------------------------------')
+print('-' * 30)
 print()
-user_input = input('What is your name?')
+user_input = input('What is your name? ')
 greeting = 'How are you '
 print(greeting + user_input)
